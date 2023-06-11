4cca323e984e3ad0467f49a9f420bd07d7e2e8bc
Melisa Ergin <ergin.melis5@gmail.com>
2023-06-12 00:52:05 +0300

    Proje dosyası ekle.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Task_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+       
+        public ActionResult Index2()
+        {
+            return View();
+        }
+       
+        public ActionResult Category(int datatag)
+        { 
+            //database operations
+            ViewBag.datatag = datatag;
+            return View();
+        }
+        public ActionResult Product()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
